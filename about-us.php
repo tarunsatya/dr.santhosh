5138480c1927813d73ee9e0a645e6a9361f0fd02
Tarun <100402462+tarunsatya@users.noreply.github.com>
2022-12-28 18:20:06 +0530

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,257 @@
+<?php
+include "includes/header.php";
+?>
+<!-- ========================
+       page title 
+    =========================== -->
+<section class="page-title page-title-layout1 bg-overlay">
+      <div class="bg-img"><img src="assets/images/page-titles/1.jpg" alt="background"></div>
+      <div class="container">
+            <div class="row">
+                  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-5">
+                        <h1 class="pagetitle__heading">Caring For The Health & Well Being Of Family.</h1>
+                        <p class="pagetitle__desc">Medcity has been present in Europe since 1990, offering innovative
+                              solutions, specializing in medical services for treatment of medical infrastructure.
+                        </p>
+                        <div class="d-flex flex-wrap align-items-center">
+                              <a href="appointment.html" class="btn btn__primary btn__rounded mr-30">
+                                    <span>Find A Doctor</span>
+                                    <i class="icon-arrow-right"></i>
+                              </a>
+                              <a href="services.html" class="btn btn__white btn__rounded">
+                                    <span>Our Services</span>
+                                    <i class="icon-arrow-right"></i>
+                              </a>
+                        </div>
+                  </div><!-- /.col-xl-5 -->
+            </div><!-- /.row -->
+      </div><!-- /.container -->
+</section><!-- /.page-title -->
+
+<!-- ========================
+      About Layout 1
+    =========================== -->
+<section class="about-layout1 pb-0">
+      <div class="container">
+            <div class="row">
+                  <div class="col-sm-12 col-md-12 col-lg-6">
+                        <div class="heading-layout2">
+                              <h3 class="heading__title mb-40">Improving The Quality Of Your Life Through Better
+                                    Health.</h3>
+                        </div><!-- /heading -->
+                  </div><!-- /.col-12 -->
+            </div><!-- /.row -->
+            <div class="row">
+                  <div class="col-sm-12 col-md-12 col-lg-6">
+                        <div class="about__Text">
+                              <p class="mb-30">Our goal is to deliver quality of care in a courteous, respectful, and
+                                    compassionate
+                                    manner. We hope you will allow us to care for you and to be the first and best
+                                    choice for healthcare.
+                              </p>
+                              <p class="mb-30">We will work with you to develop individualised care plans, including
+                                    management of
+                                    chronic diseases. We are committed to being the region’s premier healthcare network
+                                    providing patient
+                                    centered care that inspires clinical and service excellence.</p>
+                              <div class="d-flex align-items-center mb-30">
+                                    <a href="doctors-grid.html"
+                                          class="btn btn__primary btn__outlined btn__rounded mr-30">
+                                          Meet Our Doctors</a>
+                                    <img src="" alt="doctordesignation">
+                              </div>
+                        </div>
+                  </div><!-- /.col-lg-6 -->
+                  <div class="col-sm-12 col-md-12 col-lg-6">
+                        <div class="video-banner">
+                              <img src="assets/images/about/doctot2.png" alt="about">
+                              <a class="video__btn video__btn-white popup-video"
+                                    href="https://www.youtube.com/watch?v=nrJtHemSPW4">
+                                    <div class="video__player">
+                                          <i class="fa fa-play"></i>
+                                    </div>
+                              </a>
+                        </div><!-- /.video-banner -->
+                  </div><!-- /.col-lg-6 -->
+            </div><!-- /.row -->
+      </div><!-- /.container -->
+</section><!-- /.About Layout 1 -->
+
+<!-- ======================
+    Features Layout 1
+    ========================= -->
+<section class="features-layout1 pt-130 pb-50 mt--90">
+
+      <div class="container">
+            <div class="row mb-40">
+                  <div class="col-sm-12 col-md-12 col-lg-5">
+                        <div class="heading__layout2">
+                              <h3 class="heading__title">Providing Care for The Sickest In Community.</h3>
+                        </div>
+                  </div><!-- /col-lg-5 -->
+                  <div class="col-sm-12 col-md-12 col-lg-5 offset-lg-1">
+                        <p class="heading__desc font-weight-bold">Medcity has been present in Europe since 1990,
+                              offering innovative
+                              solutions, specializing in medical services for treatment of medical infrastructure. With
+                              over 100
+                              professionals actively participates in numerous initiatives aimed at creating sustainable
+                              change for
+                              patients!
+                        </p>
+                        <div class="d-flex flex-wrap align-items-center mt-40 mb-30">
+                              <a href="appointment.html" class="btn btn__primary btn__rounded mr-30">
+                                    <span>Make Appointment</span>
+                                    <i class="icon-arrow-right"></i>
+                              </a>
+                              <a href="#" class="btn btn__secondary btn__link">
+                                    <i class="icon-arrow-right icon-filled"></i>
+                                    <span>Our Core Values</span>
+                              </a>
+                        </div>
+                  </div><!-- /.col-lg-6 -->
+            </div><!-- /.row -->
+            <div class="row">
+                  <!-- Feature item #1 -->
+                  <div class="col-sm-6 col-md-6 col-lg-3">
+                        <div class="feature-item">
+                              <div class="feature__content">
+                                    <div class="feature__icon">
+                                          <i class="icon-heart"></i>
+                                          <i class="icon-heart feature__overlay-icon"></i>
+                                    </div>
+                                    <h4 class="feature__title">Medical Advices & Check Ups</h4>
+                              </div><!-- /.feature__content -->
+                              <a href="#" class="btn__link">
+                                    <i class="icon-arrow-right icon-outlined"></i>
+                              </a>
+                        </div><!-- /.feature-item -->
+                  </div><!-- /.col-lg-3 -->
+                  <!-- Feature item #2 -->
+                  <div class="col-sm-6 col-md-6 col-lg-3">
+                        <div class="feature-item">
+                              <div class="feature__content">
+                                    <div class="feature__icon">
+                                          <i class="icon-doctor"></i>
+                                          <i class="icon-doctor feature__overlay-icon"></i>
+                                    </div>
+                                    <h4 class="feature__title">Trusted Medical Treatment </h4>
+                              </div><!-- /.feature__content -->
+                              <a href="#" class="btn__link">
+                                    <i class="icon-arrow-right icon-outlined"></i>
+                              </a>
+                        </div><!-- /.feature-item -->
+                  </div><!-- /.col-lg-3 -->
+                  <!-- Feature item #3 -->
+                  <div class="col-sm-6 col-md-6 col-lg-3">
+                        <div class="feature-item">
+                              <div class="feature__content">
+                                    <div class="feature__icon">
+                                          <i class="icon-ambulance"></i>
+                                          <i class="icon-ambulance feature__overlay-icon"></i>
+                                    </div>
+                                    <h4 class="feature__title">Emergency Help Available 24/7</h4>
+                              </div><!-- /.feature__content -->
+                              <a href="#" class="btn__link">
+                                    <i class="icon-arrow-right icon-outlined"></i>
+                              </a>
+                        </div><!-- /.feature-item -->
+                  </div><!-- /.col-lg-3 -->
+                  <!-- Feature item #4 -->
+                  <div class="col-sm-6 col-md-6 col-lg-3">
+                        <div class="feature-item">
+                              <div class="feature__content">
+                                    <div class="feature__icon">
+                                          <i class="icon-drugs"></i>
+                                          <i class="icon-drugs feature__overlay-icon"></i>
+                                    </div>
+                                    <h4 class="feature__title">Medical Research Professionals </h4>
+                              </div><!-- /.feature__content -->
+                              <a href="#" class="btn__link">
+                                    <i class="icon-arrow-right icon-outlined"></i>
+                              </a>
+                        </div><!-- /.feature-item -->
+                  </div><!-- /.col-lg-3 -->
+                  <!-- Feature item #5 -->
+                  <div class="col-sm-6 col-md-6 col-lg-3">
+                        <div class="feature-item">
+                              <div class="feature__content">
+                                    <div class="feature__icon">
+                                          <i class="icon-first-aid-kit"></i>
+                                          <i class="icon-first-aid-kit feature__overlay-icon"></i>
+                                    </div>
+                                    <h4 class="feature__title">Only Qualified Doctors</h4>
+                              </div><!-- /.feature__content -->
+                              <a href="#" class="btn__link">
+                                    <i class="icon-arrow-right icon-outlined"></i>
+                              </a>
+                        </div><!-- /.feature-item -->
+                  </div><!-- /.col-lg-3 -->
+                  <!-- Feature item #6 -->
+                  <div class="col-sm-6 col-md-6 col-lg-3">
+                        <div class="feature-item">
+                              <div class="feature__content">
+                                    <div class="feature__icon">
+                                          <i class="icon-hospital"></i>
+                                          <i class="icon-hospital feature__overlay-icon"></i>
+                                    </div>
+                                    <h4 class="feature__title">Cutting Edge Facility</h4>
+                              </div><!-- /.feature__content -->
+                              <a href="#" class="btn__link">
+                                    <i class="icon-arrow-right icon-outlined"></i>
+                              </a>
+                        </div><!-- /.feature-item -->
+                  </div><!-- /.col-lg-3 -->
+                  <!-- Feature item #7 -->
+                  <div class="col-sm-6 col-md-6 col-lg-3">
+                        <div class="feature-item">
+                              <div class="feature__content">
+                                    <div class="feature__icon">
+                                          <i class="icon-expenses"></i>
+                                          <i class="icon-expenses feature__overlay-icon"></i>
+                                    </div>
+                                    <h4 class="feature__title">Affordable Prices For All Patients</h4>
+                              </div><!-- /.feature__content -->
+                              <a href="#" class="btn__link">
+                                    <i class="icon-arrow-right icon-outlined"></i>
+                              </a>
+                        </div><!-- /.feature-item -->
+                  </div><!-- /.col-lg-3 -->
+                  <!-- Feature item #8 -->
+                  <div class="col-sm-6 col-md-6 col-lg-3">
+                        <div class="feature-item">
+                              <div class="feature__content">
+                                    <div class="feature__icon">
+                                          <i class="icon-bandage"></i>
+                                          <i class="icon-bandage feature__overlay-icon"></i>
+                                    </div>
+                                    <h4 class="feature__title">Quality Care For Every Patient</h4>
+                              </div><!-- /.feature__content -->
+                              <a href="#" class="btn__link">
+                                    <i class="icon-arrow-right icon-outlined"></i>
+                              </a>
+                        </div><!-- /.feature-item -->
+                  </div><!-- /.col-lg-3 -->
+            </div><!-- /.row -->
+            <div class="row">
+                  <div class="col-md-12 col-lg-6 offset-lg-3 text-center">
+                        <p class="font-weight-bold mb-0">Serve the community by improving the quality of life through
+                              better health.
+                              We have put protocols to protect our patients and staff while continuing to provide
+                              medically necessary
+                              care.
+                              <a href="#" class="color-secondary">
+                                    <span>Contact Us For More Information</span> <i class="icon-arrow-right"></i>
+                              </a>
+                        </p>
+                  </div>
+            </div>
+      </div>
+</section>
+
+
+
+
+
+<?php
+include "includes/footer.php";
+ ?>
